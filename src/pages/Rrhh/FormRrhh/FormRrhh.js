c580b8ceaa13f0c24154224d3bcfa5bed3c52f73
develop-joseavila <86316083+develop-joseavila@users.noreply.github.com>
2021-08-13 16:20:00 -0500

    Update rrhhForm 2021-08-13
    Se actualiza el formulario con sus validaciones de los campos

--- src/pages/Rrhh/FormRrhh/FormRrhh.js
+++ src/pages/Rrhh/FormRrhh/FormRrhh.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import './formRrhh.css';
+import { Redirect } from 'react-router-dom';
+import { useFormRrhh } from './hooks/useForm.js';
+import { routes } from '../../../environments/environments.ts';
 import Select from 'react-select';
-import { useFormRrhh } from './hooks/useForm';
+
+import './formRrhh.css';
+
 
 export const FormRrhh = (props) => {
 
@@ -11,16 +15,16 @@ export const FormRrhh = (props) => {
         formValue,
         stateTitle,
         onChangeInputHandle,
-        onCheckedButtonHandle,
+        oncheckedButtonMedioRespuestaHandle,
         onChangeSelectHandle,
         onSubmitFormHandle,
-        onClickButtonRespuestaHandle
-    } = useFormRrhh(
-        {
-            descripcion: '',
-            correoEnvioRespuesta: '',
-            numeroTelefonico: '',
-        },
+        onClickButtonRespuestaHandle,
+
+    } = useFormRrhh({
+        descripcion: '',
+        correoEnvioRespuesta: '',
+        numeroTelefonico: '',
+    },
         typeForm,
     );
 
@@ -32,74 +36,82 @@ export const FormRrhh = (props) => {
         { value: 'FORMACION', label: 'FORMACIÓN' },
     ];
 
-
     return (
         <>
-
-            <div className="card mb-3">
-                <div className="bg-holder d-none d-lg-block bg-card bg-image-vum">
-                </div>
-                <div className="card-body position-relative">
-                    <div className="row">
-                        <div className="col-lg-8">
-                            <h3>{stateTitle}</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card mb-3">
-                <div className="card-body bg-light">
-                    <form onSubmit={onSubmitFormHandle}>
-                        <div className="row">
-                            {
-                                (stateTitle !== 'Talk to us')
-                                &&
-                                <div className="col-12 col-lg-4 mb-3">
-                                    <label className="form-label" htmlFor="descripcion">Categoria: </label>
-                                    <Select onChange={value => onChangeSelectHandle({ value })} options={options} placeholder={"Selecciona..."} />
-                                </div>
-                            }
-                            <div className="col-12 col-lg-8 mb-3">
-                                <label className="form-label" htmlFor="descripcion">Descripción: </label>
-                                <textarea onChange={onChangeInputHandle} value={descripcion} id="descripcion" name="descripcion" className="form-control" placeholder="Descripción"></textarea>
+            {
+                (typeForm) ?  //Si trae el tipo de formulario por el cual entra al componente
+                    <>
+                        <div className="card mb-3">
+                            <div className="bg-holder d-none d-lg-block bg-card bg-image-vum">
                             </div>
-                            <div className="col-12 col-lg-4 mb-3">
-                                <label className="form-label" htmlFor="correoEnvio">Correo envío respuesta: </label>
-                                <input onChange={onChangeInputHandle} value={correoEnvioRespuesta} id="correoEnvioRespuesta" name="correoEnvioRespuesta" className="form-control" placeholder="Correo Envío Respuesta" type="email" />
-                            </div>
-                            <div className="col-12 col-lg-4 mb-3">
-                                <label className="form-label" htmlFor="OtroMedioRespuesta">
-                                    Otro medio de respuesta: &nbsp;&nbsp;
-                                    <input onClick={onCheckedButtonHandle} className="form-check-input" id="otroMedioRespuesta" name="otroMedioRespuesta" type="checkbox" />
-                                </label>
+                            <div className="card-body position-relative">
                                 <div className="row">
-                                    <div className="col-md-7 text-right">
-                                        <input onClick={onClickButtonRespuestaHandle} className="form-check-input inputRadioOtraRespuesta" id="radioLlamadaTelefonica" type="radio" name="radioMedioRespuesta" disabled />
-                                        <label className="form-check-label" htmlFor="radioLlamadaTelefonica">Llamada teléfonica</label>
-                                    </div>
-                                    <div className="col-md-5">
-                                        <input onClick={onClickButtonRespuestaHandle} className="form-check-input inputRadioOtraRespuesta" id="radioWpp" type="radio" name="radioMedioRespuesta" disabled />
-                                        <label className="form-check-label" htmlFor="radioWpp">WhatsApp</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-lg-4 mb-3" id="divNumeroTelefonico">
-                                <label className="form-label" htmlFor="numeroTelefonico">Numero Teléfonico: </label>
-                                <input onChange={onChangeInputHandle} value={numeroTelefonico} id="numeroTelefonico" name="numeroTelefonico" className="form-control" placeholder="Numero Teléfonico" type="number" />
-                            </div>
-                            <div className="col-12 col-lg-12 mb-3">
-                                <div className="row">
-                                    <div className="col-12 col-lg-10 mb-3"></div>
-                                    <div className="col-12 col-lg-2 mb-3 d-grid gap-2">
-                                        <button type="submit" className="btn btn-success  btnEnviar">Enviar</button>
+                                    <div className="col-lg-8">
+                                        <h3>{stateTitle}</h3>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
+
+                        <div className="card mb-3">
+                            <div className="card-body bg-light">
+                                <form onSubmit={onSubmitFormHandle}>
+                                    <div className="row">
+                                        {
+                                            (stateTitle !== 'Talk to us')
+                                            &&
+                                            <div className="col-12 col-lg-4 mb-3">
+                                                <label className="form-label" htmlFor="descripcion">Categoria: </label>
+                                                <Select onChange={value => onChangeSelectHandle({ value })} options={options} placeholder={"Selecciona..."} />
+                                            </div>
+                                        }
+                                        <div className="col-12 col-lg-8 mb-3">
+                                            <label className="form-label" htmlFor="descripcion">Descripción: </label>
+                                            <textarea onChange={onChangeInputHandle} value={descripcion} id="descripcion" name="descripcion" className="form-control" placeholder="Descripción" maxLength="990" ></textarea>
+                                        </div>
+                                        <div className="col-12 col-lg-4 mb-3">
+                                            <label className="form-label" htmlFor="correoEnvio">Correo envío respuesta: </label>
+                                            <input onChange={onChangeInputHandle} value={correoEnvioRespuesta} id="correoEnvioRespuesta" name="correoEnvioRespuesta" className="form-control" placeholder="Correo Envío Respuesta" type="email" />
+                                        </div>
+                                        <div className="col-12 col-lg-4 mb-3">
+                                            <label className="form-label" htmlFor="OtroMedioRespuesta">
+                                                Otro medio de respuesta: &nbsp;&nbsp;
+                                                <input onClick={oncheckedButtonMedioRespuestaHandle} className="form-check-input" id="otroMedioRespuesta" name="otroMedioRespuesta" type="checkbox" />
+                                            </label>
+                                            <div className="row">
+                                                <div className="col-md-7 text-right">
+                                                    <input onChange={onClickButtonRespuestaHandle} data-target="llamada" className="form-check-input inputRadioOtraRespuesta" id="radioLlamadaTelefonica" type="radio" name="radioMedioRespuesta" disabled />
+                                                    <label data-target="radioLlamadaTelefonica" className="form-check-label" htmlFor="radioLlamadaTelefonica">Llamada teléfonica</label>
+                                                </div>
+                                                <div className="col-md-5">
+                                                    <input onChange={onClickButtonRespuestaHandle} data-target="wpp" className="form-check-input inputRadioOtraRespuesta" id="radioWpp" type="radio" name="radioMedioRespuesta" disabled />
+                                                    <label data-target="radioWpp" className="form-check-label" htmlFor="radioWpp">WhatsApp</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-12 col-lg-4 mb-3" id="divNumeroTelefonico">
+                                            <label className="form-label" htmlFor="numeroTelefonico">Numero Teléfonico: </label>
+                                            <input onChange={onChangeInputHandle} value={numeroTelefonico} id="numeroTelefonico" name="numeroTelefonico" className="form-control" placeholder="Numero Teléfonico" type="number" />
+                                        </div>
+                                        <div className="col-12 col-lg-12 mb-3">
+                                            <div className="row">
+                                                <div className="col-12 col-lg-10 mb-3"></div>
+                                                <div className="col-12 col-lg-2 mb-3 d-grid gap-2">
+                                                    <button type="submit" className="btn btn-success  btnEnviar">Enviar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </>
+
+                    :
+
+                    <Redirect to={routes.rrhh.url} /> //Si no trae el tipo e formulario se redirecciona al page padre de RRHH
+            }
 
         </>
     )
--- src/pages/Rrhh/FormRrhh/hooks/useForm.js
+++ src/pages/Rrhh/FormRrhh/hooks/useForm.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/dist/sweetalert2.css';
 
 export const useFormRrhh = (formInitialState = {}, typeForm,) => {
+
+    const exprRegTelefono = /^[0-9+() -?]+$/;                                                //Expresion regular para validar el formato de un teléfono
+    const exprRegEmail = /^([a-zA-Z0-9_.-]+)@([\da-zA-Z0-9.-]+)\.([a-z.]{2,6})$/;        //Expresion regular para validar los correos electronicos
+    const exprRegTexto = /([\w\s\d\nÑñáéíóúÁÉÍÓÚ.,\-_@?¿"%'<>]){0,990}/;                      //Expresion regular para validar texto largos de 1000 caracteres
 
     useEffect(() => {
         document.getElementById('top').className = 'main dashboard rrhhForm';
@@ -45,18 +51,28 @@ export const useFormRrhh = (formInitialState = {}, typeForm,) => {
     }
 
 
-    const onCheckedButtonHandle = () => {
+    const [checkedButtonMedioRespuesta, setcheckedButtonMedioRespuesta] = useState(null)
+    const oncheckedButtonMedioRespuestaHandle = () => {
         let otroMedioRespuesta = document.getElementById("otroMedioRespuesta");
 
         if (otroMedioRespuesta.checked) {
+            setcheckedButtonMedioRespuesta(true);
             document.getElementById("radioLlamadaTelefonica").disabled = false;
             document.getElementById("radioWpp").disabled = false;
             document.getElementById("divNumeroTelefonico").style.display = 'unset';
         } else {
-            document.getElementById("radioLlamadaTelefonica").disabled = true;
-            document.getElementById("radioLlamadaTelefonica").checked = false;
+            setcheckedButtonMedioRespuesta(false);
+            setTipoContacto(null);
 
+            setStateForm({
+                ...formValue,
+                'numeroTelefonico': ''
+            });
+
+            document.getElementById("radioLlamadaTelefonica").disabled = true;
             document.getElementById("radioWpp").disabled = true;
+
+            document.getElementById("radioLlamadaTelefonica").checked = false;
             document.getElementById("radioWpp").checked = false;
 
             document.getElementById("divNumeroTelefonico").style.display = 'none';
@@ -64,7 +80,7 @@ export const useFormRrhh = (formInitialState = {}, typeForm,) => {
 
     }
 
-    const [selectValue, setSelect] = useState({})
+    const [selectCategorias, setSelect] = useState({})
     const onChangeSelectHandle = ({ value }) => {
         setSelect(value);
     }
@@ -72,29 +88,103 @@ export const useFormRrhh = (formInitialState = {}, typeForm,) => {
     const onSubmitFormHandle = (e) => {
         e.preventDefault();
 
-        let dataForm = {
-            categoria: selectValue.value,
-            descripcion: formValue.descripcion,
-            correoEnvioRespuesta: formValue.correoEnvioRespuesta,
-            numeroTelefonico: formValue.numeroTelefonico,
-        }
+        if (validarCampos()) {
 
-        console.log("formValue", dataForm);
+            let dataForm = {
+                categoria: selectCategorias.value,
+                descripcion: formValue.descripcion,
+                correoEnvioRespuesta: formValue.correoEnvioRespuesta,
+                numeroTelefonico: formValue.numeroTelefonico,
+                formulario: typeForm.typeForm,
+                tipoContacto: tipoContacto
+            }
+            console.log("formValue", dataForm);
+
+            Swal.fire({
+                title: 'Datos guardados correctamente',
+                icon: 'success',
+                confirmButtonText: 'Ok',
+                confirmButtoncolor: "#1783EE",
+            });
+
+        } else {
+            Swal.fire({
+                title: 'Advertencia',
+                html: `
+                    <div className="row">
+                        <div className="col-12 col-lg-12" style="text-align: left; font-size: 16px; font-weight: 600; margin-bottom: 15px;">
+                            Revisa el formulario con las siguientes indicacones:
+                        </div>
+                        <div className="col-12 col-lg-12" style="text-align: left; font-size: 15px;">
+                            ◉ Los campos no deben estar vacios <br/>
+                            ◉ No se permiten carácteres especiales Eje: !"#$%&/() <br/>
+                        </div>
+                    </div>
+                     <br/>
+                    
+                `,
+                icon: 'warning',
+                confirmButtonText: 'Cerrar',
+                confirmButtoncolor: "#1783EE",
+            });
+        }
     }
 
-    const [checkedValue, setCheckedValue] = useState(formInitialState);
+
+    const validarCampos = () => {
+
+        let response = false;
+
+
+        if (
+            exprRegTexto.test(formValue.descripcion) &&    //Si el texto de la descripcion cumple con la expresion regular
+            exprRegEmail.test(formValue.correoEnvioRespuesta)   //Si el correo cumple con la expresion regular
+        ) {
+
+            console.log("typeForm", typeForm);
+
+            if (
+                selectCategorias.value === undefined && //Si el formulario es undefined
+                typeForm.typeForm !== "talkToUs" //Si el formulario es diferente de talkToUs
+            ) {
+                response = false;
+            } else {
+                response = true;
+            }
+
+            if (checkedButtonMedioRespuesta === true && tipoContacto === null) { //Si Otro medio de respuesta esta checkeado y no se ha seleccionado el tipo de contacto
+                response = false;
+            } else if (checkedButtonMedioRespuesta === true && tipoContacto !== null) { //Si Otro medio de respuesta esta checkeado y se ha seleccionado el tipo de contacto
+                (exprRegTelefono.test(formValue.numeroTelefonico)) ? response = true : response = false; //Si el formato del telefono cumple  = true
+            }
+
+        }
+
+        return response;
+
+    }
+
+
+    const [tipoContacto, setTipoContacto] = useState(null);
     const onClickButtonRespuestaHandle = ({ target }) => {
-        // setCheckedValue({
-        //     ...formValue,
-        //     [target.name]: target.value
-        // })
+
+        let inputRadio = document.querySelector("#" + target.id);
+
+        if (inputRadio.checked && target.dataset.target === "wpp") {
+            setTipoContacto("wpp");
+        } else if (inputRadio.checked && target.dataset.target === "llamada") {
+            setTipoContacto("llamada");
+        } else {
+            setTipoContacto(null);
+        }
+
     }
 
     return ({
         formValue,
         stateTitle,
         onChangeInputHandle,
-        onCheckedButtonHandle,
+        oncheckedButtonMedioRespuestaHandle,
         onChangeSelectHandle,
         onSubmitFormHandle,
         onClickButtonRespuestaHandle
